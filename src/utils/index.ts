8dce45e68c2e9b7036e9eef236e2bc9d0f1a1250
n4rvs3 <n4rvs3apex@gmail.com>
2024-02-01 19:08:54 +0900

    feat: 月日のバリデーションの追加

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,11 @@ export { Era } from './era';
 export const isValidEightDigitNumber = (value: number): boolean => {
   return value >= 10000000 && value <= 99999999;
 };
+
+export const isValidMonth = (value: number): boolean => {
+  return value >= 1 && value <= 12;
+}
+
+export const isValidDate = (value: number): boolean => {
+  return value >= 1 && value <= 31;
+}
