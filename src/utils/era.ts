dbc57570255a65cd69de8a25855a4ffde33b94f1
n4rvs3 <n4rvs3apex@gmail.com>
2024-02-01 16:39:12 +0900

    feat: 和暦の範囲及び読み仮名を纏めた変数の追加

---
+++ src/utils/era.ts
@@ -0,0 +1,28 @@
+import type { EraType } from "../types";
+
+export const Era: EraType[] = [
+  {
+    start: 19120730,
+    end: 19251225,
+    name: "大正",
+    nameKana: "たいしょう",
+  },
+  {
+    start: 19251226,
+    end: 19890107,
+    name: "昭和",
+    nameKana: "しょうわ",
+  },
+  {
+    start: 19890108,
+    end: 20190430,
+    name: "平成",
+    nameKana: "へいせい",
+  },
+  {
+    start: 20190501,
+    end: 20341231,
+    name: "令和",
+    nameKana: "れいわ",
+  },
+];
