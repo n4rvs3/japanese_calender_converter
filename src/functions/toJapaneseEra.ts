519dd8c4e5cbaf20838ac2558b425d41db6362b6
n4rvs3 <n4rvs3apex@gmail.com>
2024-02-01 17:56:10 +0900

    feat: 西暦から和暦へ変換時にオブジェクトで返すように変更 また、与えられた引数が8桁であるかのバリデーションを追加

--- src/functions/toJapaneseEra.ts
+++ src/functions/toJapaneseEra.ts
@@ -1,19 +1,44 @@
-import { Era } from '../utils';
+import { Era, isValidEightDigitNumber } from '../utils';
+import { DetailedEraInfo } from '../types/era';
+
+const calcYear = (yyyymmdd: number, start: number): number => {
+  const fullYear = parseInt(yyyymmdd.toString().slice(0, 4), 10);
+  const eraStartYear = parseInt(start.toString().slice(0, 4), 10);
+  const year = fullYear - eraStartYear + 1;
+  return year;
+};
+
+const calcMonth = (yyyymmdd: number): number => {
+  const month = parseInt(yyyymmdd.toString().slice(4, 6), 10);
+  return month;
+};
+
+const calcDate = (yyyymmdd: number): number => {
+  const date = parseInt(yyyymmdd.toString().slice(6, 8), 10);
+  return date;
+};
 
 // 西暦を和暦に変換する関数
-export const toJapaneseEra = (yymmdd: number): string => {
+export const toJapaneseEra = (yyyymmdd: number): DetailedEraInfo => {
+  if (!isValidEightDigitNumber(yyyymmdd)) {
+    throw new Error('yyyymmdd must be an 8-digit number.');
+  }
+
   const era = Era;
+  let japaneseEra: DetailedEraInfo = {} as DetailedEraInfo;
 
-  let japaneseEra = '不明';
-
-  // eraをループで回す
-  // era.startとera.endの間にyymmddがあれば、そのera.nameを返す
-
-  // 1. eraをループで回す
   for (let i = 0; i < era.length; i++) {
-    // 2. era.startとera.endの間にyymmddがあれば、そのera.nameを返す
-    if (yymmdd >= era[i].start && yymmdd <= era[i].end) {
-      japaneseEra = era[i].name;
+    if (yyyymmdd >= era[i].start && yyyymmdd <= era[i].end) {
+      const tmpEra = era[i].name;
+      const tmpYear = calcYear(yyyymmdd, era[i].start);
+      const tmpMonth = calcMonth(yyyymmdd);
+      const tmpDate = calcDate(yyyymmdd);
+      return (japaneseEra = {
+        era: tmpEra,
+        year: tmpYear,
+        month: tmpMonth,
+        date: tmpDate,
+      });
     }
   }
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,5 @@
-export { Era } from "./era";
+export { Era } from './era';
+
+export const isValidEightDigitNumber = (value: number): boolean => {
+  return value >= 10000000 && value <= 99999999;
+};
