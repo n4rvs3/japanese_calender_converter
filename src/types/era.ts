25e61f876ffede65ad99bf9e82ef543ffe49bd26
n4rvs3 <n4rvs3apex@gmail.com>
2024-02-01 17:54:37 +0900

    feat: 明治時代に対応

--- src/types/era.ts
+++ src/types/era.ts
@@ -1,6 +1,13 @@
 export type EraType = {
   start: number;
   end: number;
-  name: string;
-  nameKana: string;
+  name: '明治' | '大正' | '昭和' | '平成' | '令和';
+  nameKana: 'めいじ' | 'たいしょう' | 'しょうわ' | 'へいせい' | 'れいわ';
 };
+
+export interface DetailedEraInfo {
+  era: '明治' | '大正' | '昭和' | '平成' | '令和';
+  year: number;
+  month: number;
+  date: number;
+}
--- src/utils/era.ts
+++ src/utils/era.ts
@@ -1,28 +1,34 @@
-import type { EraType } from "../types";
+import type { EraType } from '../types';
 
 export const Era: EraType[] = [
   {
+    start: 18680125,
+    end: 19120729,
+    name: '明治',
+    nameKana: 'めいじ',
+  },
+  {
     start: 19120730,
     end: 19251225,
-    name: "大正",
-    nameKana: "たいしょう",
+    name: '大正',
+    nameKana: 'たいしょう',
   },
   {
     start: 19251226,
     end: 19890107,
-    name: "昭和",
-    nameKana: "しょうわ",
+    name: '昭和',
+    nameKana: 'しょうわ',
   },
   {
     start: 19890108,
     end: 20190430,
-    name: "平成",
-    nameKana: "へいせい",
+    name: '平成',
+    nameKana: 'へいせい',
   },
   {
     start: 20190501,
     end: 20341231,
-    name: "令和",
-    nameKana: "れいわ",
+    name: '令和',
+    nameKana: 'れいわ',
   },
 ];
